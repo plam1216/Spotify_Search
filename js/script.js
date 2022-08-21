fe17c291975c9a8d26dcc8ca72ac6f31dbf3b1cb
Peter Lam <plam1216@gmail.com>
2022-08-20 22:54:29 -0400

    display album artwork and album name

--- js/script.js
+++ js/script.js
@@ -5,36 +5,29 @@
 // podcasts, albums, playlists, etc. are not searched because url is set to search only 'artist'
 
 // topSearch is displaying all resuilts, but I only will be getting the top result artist ID
-const topSearch = {
+
+// drake 3TVXtAsR1Inumwj472S9r4
+let topSearch = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spotify23.p.rapidapi.com/search/?q=DRAKE&type=artist&offset=0&limit=10&numberOfTopResults=5",
-	// "url": "https://spotify23.p.rapidapi.com/search/?q='artistName'&type=artist&offset=0&limit=10&numberOfTopResults=5",
+	"url": "https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=%3CREQUIRED%3E&offset=0&limit=10&numberOfTopResults=5",
 	"method": "GET",
 	"headers": {
-		"X-RapidAPI-Key": "9c7b7aea76msh939670bd686fbccp1a2909jsnf3ab8af69b4b",
+		"X-RapidAPI-Key": "0f340b579bmsheb5c8cf361160b1p1e6216jsn8547c0d51260",
 		"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
 	}
 };
 
-
-const albums = {
+let albums = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spotify23.p.rapidapi.com/artist_albums/?id=3TVXtAsR1Inumwj472S9r4&offset=0&limit=100",
-	// "url": "https://spotify23.p.rapidapi.com/artist_albums/?id=artistID&offset=0&limit=100",
+	"url": "https://spotify23.p.rapidapi.com/artist_albums/?id=%3CREQUIRED%3E&offset=0&limit=100",
 	"method": "GET",
 	"headers": {
-		"X-RapidAPI-Key": "9c7b7aea76msh939670bd686fbccp1a2909jsnf3ab8af69b4b",
+		"X-RapidAPI-Key": "0f340b579bmsheb5c8cf361160b1p1e6216jsn8547c0d51260",
 		"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
 	}
 };
-
-// $.ajax(albums).done(function (response) {
-// 	console.log(response);
-// });
-
-
 
 			// GET DISCOGRAPHY OVERVIEW
 // const overview = {
@@ -64,21 +57,35 @@ $form.on('submit', handleSubmit)
 // .then() has a fail case
 // .done() would be called when object resolves, wouldn't show the error if there is one
 
+// function getArtistUrl(inputVal) {
+	// topSearch.url = `url": "https://spotify23.p.rapidapi.com/search/?q=${inputVal}&type=%artist&offset=0&limit=10&numberOfTopResults=5`
+	
+// 	console.log('getartist', topSearch)
+// }
+
 
 function handleSubmit(event){ 
 	event.preventDefault()
 	
+	$discography.empty()
+	let inputVal = $input.val()
+
+	topSearch.url = `https://spotify23.p.rapidapi.com/search/?q=${inputVal}&type=%artist&offset=0&limit=10&numberOfTopResults=5`
+
+	// getArtistUrl(inputVal)
+	
+	console.log('handle', topSearch)
 	$.ajax(topSearch).then(
 		function(data){
 			// console.log(data)
-			console.log(data.artists.items[0].data.uri)
+			console.log('Artist URI', data.artists.items[0].data.uri)
 			// returns something like this spotify:artist:3TVXtAsR1Inumwj472S9r4
 			// split by :
-			// index 2 to get ONLY artist ID
+			// index 2 to get ONLY artist ID 
 			let artistID = data.artists.items[0].data.uri.split(':')[2]
 			console.log('Artist ID: ',artistID)
 
-			getDiscography()
+			getDiscography(artistID)
 		},
 		function(error){
 			console.log('something went wrong...')
@@ -86,12 +93,18 @@ function handleSubmit(event){
 	)
 }
 	
-function getDiscography(){
+function getDiscography(artistID){
+
+	albums.url = `https://spotify23.p.rapidapi.com/artist_albums/?id=${artistID}&offset=0&limit=100`
+	
 	$.ajax(albums).then(
 		function(data){
-			// console.log(data)
+			console.log(data)
 			// gets one album
+			// Spotify always stores the album name at index 0
 			console.log('Album: ', data.data.artist.discography.albums.items[0].releases.items[0].name)
+			// theres 3 images, 300x300, 64x64, 600x600
+			console.log('Artwork: ', data.data.artist.discography.albums.items[0].releases.items[0].coverArt.sources[0].url)
 			// console.log(data.data.artist.discography.albums.items)
 
 			//  assigns array of albums to albums variable
@@ -101,9 +114,10 @@ function getDiscography(){
 			// iterate through array using jQuery to get each album name
 			// Spotify always stores the album name at index 0
 			$.each(albums, function(index, value) {
-				let albumName = value.releases.items[0].name;
-				// console.log(albumName)
-				$discography.append(`<p>${albumName}</p>`)
+				let albumName = value.releases.items[0].name;				
+				let coverArt = value.releases.items[0].coverArt.sources[0].url
+
+				$discography.append(`<p><img src='${coverArt}'></img>${albumName}</p>`)
 			})
 
 			// data.data.artist.discography.albums.items.each(item, function(){
